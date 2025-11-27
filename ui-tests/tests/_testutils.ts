4ebf21e8aba341794e9f208851056bb74e300bfd
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-11-27 10:07:59 -0600

    more ci, test

--- ui-tests/tests/_testutils.ts
+++ ui-tests/tests/_testutils.ts
@@ -7,7 +7,7 @@ export async function advanceTour(
   stop?: number,
   start: number = 1
 ) {
-  stop = stop || steps;
+  stop = stop || steps - 1;
   for (let i = start; i <= stop; i++) {
     await page.getByLabel(`Next (Step ${i} of ${steps})`, { exact: true }).click();
   }
